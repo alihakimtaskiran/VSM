df52fe59a5623c2076e132177330c26d04a07e3e
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2019-07-23 16:58:20 +0300

    Update main.py

--- Cosine Similarity/main.py
+++ Cosine Similarity/main.py
@@ -1,12 +1,12 @@
-import backend as b
+import vsm
 
 doc1="Su yerken boğuldum."
 doc2="Su uyur düşman uyumaz."
 
-b.import_contents(doc1)
-b.import_contents(doc2)
+vsm.import_contents(doc1)
+vsm.import_contents(doc2)
 
-v1=b.func.doc2vec(doc1)
-v2=b.func.doc2vec(doc2)
+v1=vsm.func.doc2vec(doc1)
+v2=vsm.func.doc2vec(doc2)
 
-print(b.func.cossim(v1,v2))
+print(vsm.func.cossim(v1,v2))
