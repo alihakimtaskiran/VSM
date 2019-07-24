d069c9edbd806fbe624bc7617f6ce0d03e132acb
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2019-07-24 14:54:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import vsm
 
-doc1="Su yerken boğuldum."
-doc2="Su uyur düşman uyumaz."
-
-vsm.import_contents(doc1)
-vsm.import_contents(doc2)
+doc1="Su yerken boğuldum düşmanlık."
+doc2="Susuz uyur düşman uyumaz."
+doc3="你好"
+doc4="你"
+vsm.import_contents(doc1,0)
+vsm.import_contents(doc2,0)
+vsm.import_contents(doc3,1)
+vsm.import_contents(doc4,1)
 
 v1=vsm.func.doc2vec(doc1)
 v2=vsm.func.doc2vec(doc2)
 
+v3=vsm.func.doc2vec(doc3)
+v4=vsm.func.doc2vec(doc4)
+
 print(vsm.func.cossim(v1,v2))
+print(vsm.func.cossim(v3,v4))
